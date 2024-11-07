b35448cca48f038c6552d47893cc05bade10f4dc
Ali1Rhm <gamecodein@gmail.com>
2024-11-08 01:42:54 +0330

    Update 04

--- Adaline_XO/Program.cs
+++ Adaline_XO/Program.cs
@@ -2,9 +2,11 @@
 
 #region XO Patterns
 
-List<(float[] inputs, int label)> patterns = new();
+List<(float[] inputs, int label)> train_patterns = new();
+List<(float[] inputs, int label)> test_patterns = new();
 
-foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData.txt"))
+
+foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData10Train.txt"))
 {
     string[] values = line.Split(new char[] { ' ' });
 
@@ -13,21 +15,34 @@ foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData.txt"))
     for (int i = 0; i < inputs.Length; i++)
         inputs[i] = float.Parse(values[i]);
 
-    patterns.Add((inputs, label));
+    train_patterns.Add((inputs, label));
+}
+
+foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData10Test.txt"))
+{
+    string[] values = line.Split(new char[] { ' ' });
+
+    int label = int.Parse(values[^1]);
+    float[] inputs = new float[values.Length - 1];
+    for (int i = 0; i < inputs.Length; i++)
+        inputs[i] = float.Parse(values[i]);
+
+    test_patterns.Add((inputs, label));
 }
 
 #endregion
 
 #region Apply Adaline Algorithm
 
-const int col = 5;
-const int row = 5;
+const int col = 10;
+const int row = 10;
 const int data_size = col * row;
 
-float alpha = 0.1f;
-float deltaThreshold = 0.1f;
+float alpha = 0.01f;
+float deltaThreshold = 0.01f;
 float[] w = new float[data_size];
 float bias = 0;
+float theta = 0f;
 
 float totalError = 0;
 int iteration = 0;
@@ -46,7 +61,7 @@ void Adaline(ref float totalError)
 {
     stop = true;
 
-    foreach ((var inputs, var label) in patterns)
+    foreach ((var inputs, var label) in train_patterns)
     {
         float yni = 0;
 
@@ -58,7 +73,7 @@ void Adaline(ref float totalError)
 
         float error = label - yni;
         totalError += error * error;
-        int prediction = StepFunction(yni, 0f);
+        int prediction = StepFunction(yni, theta);
 
         if (prediction == label)
             continue;
@@ -100,7 +115,12 @@ Console.WriteLine(equataion_string + "\n");
 
 #region Test
 
-foreach ((var inputs, var label) in patterns)
+int truePositive = 0;
+int trueNegative = 0;
+int falsePositive = 0;
+int falseNegative = 0;
+
+foreach ((var inputs, var label) in test_patterns)
 {
     float yni = 0;
     int prediction;
@@ -111,12 +131,38 @@ foreach ((var inputs, var label) in patterns)
     }
     yni += bias;
 
-    prediction = StepFunction(yni, 0.2f);
+    prediction = StepFunction(yni, theta);
 
     DrawLetter(inputs);
 
     Console.WriteLine((prediction == 0 ? "Not Defined" : (prediction == 1) ? "X" : "O") + "\n");
+
+    if (prediction == label && label == 1)
+        truePositive++;
+    else if (prediction == label && label == -1)
+        trueNegative++;
+    else if (prediction != label && label == 1)
+        falseNegative++;
+    else if(prediction != label && label == -1)
+        falsePositive++;
 }
+
+double precision = truePositive / (double)(truePositive + falsePositive);
+double recall = truePositive / (double)(truePositive + falseNegative);
+double accuracy = (truePositive + trueNegative) / (double)(truePositive + trueNegative + falsePositive + falseNegative);
+double f1Score = 2 * (precision * recall) / (precision + recall);
+
+// Print the confusion matrix and metrics
+Console.WriteLine("Confusion Matrix:");
+Console.WriteLine($"                 Predicted X   Predicted O");
+Console.WriteLine($"Actual X         {truePositive}            {falseNegative}");
+Console.WriteLine($"Actual O         {falsePositive}            {trueNegative}\n");
+
+Console.WriteLine("Metrics:");
+Console.WriteLine($"Accuracy : {accuracy:P2}");
+Console.WriteLine($"Precision: {precision:P2}");
+Console.WriteLine($"Recall   : {recall:P2}");
+Console.WriteLine($"F1 Score : {f1Score:P2}");
 
 #endregion
 
@@ -124,11 +170,11 @@ foreach ((var inputs, var label) in patterns)
 
 static void DrawLetter(float[] letter)
 {
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < col; j++)
         {
-            Console.Write(letter[5 * i + j] == 1 ? "# " : ". ");
+            Console.Write(letter[row * i + j] == 1 ? "# " : ". ");
         }
         Console.WriteLine();
     }
--- Perceptron_XO/Program.cs
+++ Perceptron_XO/Program.cs
@@ -2,9 +2,11 @@
 
 #region XO Patterns
 
-List<(float[] inputs, int label)> patterns = new();
+List<(float[] inputs, int label)> train_patterns = new();
+List<(float[] inputs, int label)> test_patterns = new();
 
-foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData.txt"))
+
+foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData10Train.txt"))
 {
     string[] values = line.Split(new char[] { ' ' });
 
@@ -13,21 +15,33 @@ foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData.txt"))
     for (int i = 0; i < inputs.Length; i++)
         inputs[i] = float.Parse(values[i]);
 
-    patterns.Add((inputs, label));
+    train_patterns.Add((inputs, label));
+}
+
+foreach (var line in File.ReadLines(@"C:\Dev\ANN\XOData10Test.txt"))
+{
+    string[] values = line.Split(new char[] { ' ' });
+
+    int label = int.Parse(values[^1]);
+    float[] inputs = new float[values.Length - 1];
+    for (int i = 0; i < inputs.Length; i++)
+        inputs[i] = float.Parse(values[i]);
+
+    test_patterns.Add((inputs, label));
 }
 
 #endregion
 
 #region Apply Perceptron Algorithm
 
-const int col = 5;
-const int row = 5;
+const int col = 10;
+const int row = 10;
 const int data_size = col * row;
 
 float[] w = new float[data_size];
 float bias = 0;
-float alpha = 1f;
-float theta = 0.2f;
+float alpha = 0.1f;
+float theta = 0f;
 
 int iteration = 0;
 bool stop = false;
@@ -44,7 +58,7 @@ void Perceptron(int iteration = 1)
 {
     stop = true;
 
-    foreach ((var inputs, var label) in patterns)
+    foreach ((var inputs, var label) in train_patterns)
     {
         float yni = 0;
 
@@ -94,7 +108,12 @@ Console.WriteLine(equataion_string+"\n");
 
 #region Test
 
-foreach ((var inputs, var label) in patterns)
+int truePositive = 0;
+int trueNegative = 0;
+int falsePositive = 0;
+int falseNegative = 0;
+
+foreach ((var inputs, var label) in test_patterns)
 {
     float yni = 0;
     int prediction;
@@ -110,7 +129,33 @@ foreach ((var inputs, var label) in patterns)
     DrawLetter(inputs);
 
     Console.WriteLine((prediction == 0 ? "Not Defined" : (prediction == 1) ? "X" : "O") + "\n");
+
+    if (prediction == label && label == 1)
+        truePositive++;
+    else if (prediction == label && label == -1)
+        trueNegative++;
+    else if (prediction != label && label == 1)
+        falseNegative++;
+    else if (prediction != label && label == -1)
+        falsePositive++;
 }
+
+double precision = truePositive / (double)(truePositive + falsePositive);
+double recall = truePositive / (double)(truePositive + falseNegative);
+double accuracy = (truePositive + trueNegative) / (double)(truePositive + trueNegative + falsePositive + falseNegative);
+double f1Score = 2 * (precision * recall) / (precision + recall);
+
+// Print the confusion matrix and metrics
+Console.WriteLine("Confusion Matrix:");
+Console.WriteLine($"                 Predicted X   Predicted O");
+Console.WriteLine($"Actual X         {truePositive}            {falseNegative}");
+Console.WriteLine($"Actual O         {falsePositive}            {trueNegative}\n");
+
+Console.WriteLine("Metrics:");
+Console.WriteLine($"Accuracy : {accuracy:P2}");
+Console.WriteLine($"Precision: {precision:P2}");
+Console.WriteLine($"Recall   : {recall:P2}");
+Console.WriteLine($"F1 Score : {f1Score:P2}");
 
 #endregion
 
@@ -118,11 +163,11 @@ foreach ((var inputs, var label) in patterns)
 
 static void DrawLetter(float[] letter)
 {
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0; j < 5; j++)
+        for (int j = 0; j < col; j++)
         {
-            Console.Write(letter[5*i + j] == 1 ? "# " : ". ");
+            Console.Write(letter[row * i + j] == 1 ? "# " : ". ");
         }
         Console.WriteLine();
     }
